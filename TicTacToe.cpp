5a489e0ae80c0a5278b41c37821a1c3ff9710e20
picabotpi <59852986+picabotpi@users.noreply.github.com>
2020-09-24 15:57:55 -0600

    Update TicTacToe.cpp

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -69,6 +69,7 @@ int main(){
                 DisplayBoard(arr);
             }
             else{
+                cout << "Out of Moves" << endl;
                 DisplayBoard(arr);
                 return 0;
             }
